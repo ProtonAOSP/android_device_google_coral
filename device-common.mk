346ab89a05d3f04e0668c25fe42d816045eb792e
Danny Lin <danny@kdrag0n.dev>
2020-10-30 12:57:00 -0700

    floral: Enable compositor background blurring
    This allows us to create a "glass blur" look throughout the system for
    improved visuals.
    
    Change-Id: I63fd67e403cc11463f5a95baab3088bc549d8149

--- device-common.mk
+++ device-common.mk
@@ -123,4 +123,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.incremental.enable=module:/vendor/lib/modules/incrementalfs.ko
 
+# Enable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1
+
 PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true

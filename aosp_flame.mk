6b438a635064eb3017789aac66bad421c47a16c5
Danny Lin <danny@kdrag0n.dev>
2021-04-07 00:37:55 -0700

    Inherit extracted proprietary blobs
    coral provides the base set of blobs for both devices. The build system
    uses the first item in PRODUCT_COPY_FILES if there are duplicates, so
    include flame's own blobs before coral as an override.
    
    Change-Id: I4bda9a89c9d1fdfe3a38d83695cf7f6d5d9a9c74

--- aosp_flame.mk
+++ aosp_flame.mk
@@ -44,6 +44,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, device/google/coral/device-flame.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/proprietary/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/google_devices/coral/prebuilts/device-vendor-flame.mk)
+$(call inherit-product-if-exists, vendor/google/flame/flame-vendor.mk)
+$(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml

3cc0931143f00fd974da95aa2888bd1cda791d9d
Danny Lin <danny@kdrag0n.dev>
2021-06-07 23:04:54 -0700

    Revert "floral: Switch to Skia's Vulkan rendering backend"
    This reverts commit bb393c6cf03dd0cd4062731e709ea90d37f78430.
    
    In some media-heavy apps, using the Vulkan backend for Skia UI rendering
    causes fd leaks after ~15 minutes (e.g. long video call sessions and
    prolonged usage of TikTok):
    
    E Parcel  : fcntl(F_DUPFD_CLOEXEC) failed in Parcel::read, i is 1, fds[i] is -1, fd_count is 2, error: Too many open files
    E Surface : dequeueBuffer: IGraphicBufferProducer::requestBuffer failed: -22
    W OpenGLRenderer: dequeueBuffer failed, error = -22; switching to fallback
    E qdgralloc: importBuffer: Unable to clone handle
    E GraphicBufferAllocator: Failed to allocate (1 x 1) layerCount 1 format 1 usage 10000b00: 5
    D GraphicBufferAllocator: GraphicBufferAllocator buffers:
    D GraphicBufferAllocator: 0x7918b4ed50: unknown     |  480 ( 512) x  864 |    1 | 7FA30C06 | 0x20000900 | SurfaceTexture-1-15218-21
    D GraphicBufferAllocator: 0x7918bf8130: 15792.00 KiB | 1480 (1536) x 2632 |    1 |        2 | 0x10000100 | SurfaceTexture-0-15218-103
    D GraphicBufferAllocator: 0x7918c05d20: 15792.00 KiB | 1480 (1536) x 2632 |    1 |        2 | 0x10000100 | SurfaceTexture-0-15218-110
    D GraphicBufferAllocator: 0x7918c12d40: unknown     |  576 ( 640) x 1024 |    1 | 7FA30C06 | 0x20000900 | SurfaceTexture-1-15218-1
    D GraphicBufferAllocator: 0x7918c13ac0: 15792.00 KiB | 1480 (1536) x 2632 |    1 |        2 | 0x10000100 | SurfaceTexture-0-15218-110
    D GraphicBufferAllocator: Total allocated by GraphicBufferAllocator (estimate): 47376.00 KB
    D GraphicBufferAllocator: -------------------------------
    D GraphicBufferAllocator: QTI gralloc dump:
    D GraphicBufferAllocator: -------------------------------
    D GraphicBufferAllocator: -------------------------------
    E AHardwareBuffer: GraphicBuffer(w=1, h=1, lc=1) failed (Out of memory), handle=0x0
    W OpenGLRenderer: Failed to allocate scratch buffer, error=-12
    E OpenGLRenderer: dequeueBuffer failed: Function not implemented (-38)
    E OpenGLRenderer: VulkanSurface::dequeueNativeBuffer called with an invalid surface!
    
    Once the app exhausts the per-UID fd limit, videos freeze and the app
    crashes in some cases.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I355d14e81366bb59ff760e0e5be03ab6de704bd7

--- device-common.mk
+++ device-common.mk
@@ -140,10 +140,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 
-# Use Vulkan backend for Skia UI rendering
-PRODUCT_PRODUCT_PROPERTIES += \
-    debug.hwui.renderer=skiavk
-
 # Enable missing props
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.opa.eligible_device=true

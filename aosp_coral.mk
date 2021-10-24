4eb0c8daac51c421a3be09d6936f72ceaf29454e
Danny Lin <danny@kdrag0n.dev>
2021-10-24 03:10:36 -0700

    coral: Update fingerprint from SP1A.210812.016.A1
    Change-Id: I9736e86d210c42a2cfca196195c4ef074e3547f1

--- aosp_coral.mk
+++ aosp_coral.mk
@@ -71,4 +71,4 @@ PRODUCT_DEVICE := coral
 PRODUCT_MODEL := Pixel 4 XL
 
 # Match stock build fingerprint
-PRODUCT_OVERRIDE_FINGERPRINT := google/coral/coral:12/SPB5.210812.002/7671067:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT := google/coral/coral:12/SP1A.210812.016.A1/7796139:user/release-keys
